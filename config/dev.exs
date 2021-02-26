2280dd147c33ef2de5050a7bcca315b34fa1b435
robisnei <robisneey@hotmail.com>
2021-02-26 19:51:41 -0300

    update db pass

--- config/dev.exs
+++ config/dev.exs
@@ -3,9 +3,9 @@ use Mix.Config
 # Configure your database
 config :robspay, Robspay.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "PASSWORD",
   database: "robspay_dev",
-  hostname: "localhost",
+  hostname: "postgres13",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
